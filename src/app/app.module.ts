13ff81a07494ba040229c0e3554156272209cff3
Innessa Schwartz <innessa.schwartz@gmail.com>
2018-07-13 16:47:44 -0500

    Everything.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MyRecipesComponent } from './my-recipes/my-recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
+const routes: Routes = [
+	{path:'', component:HomeComponent},
+	{path:'my-recipes', component:MyRecipesComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		HomeComponent,
+		MyRecipesComponent,
+		RecipeListComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot(routes),
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
